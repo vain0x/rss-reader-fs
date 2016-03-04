594e023ea40aa71d0c69fb9c1b6bbb6a4ce6c603
vain0 <id_vain@yahoo.co.jp>
2016-03-04 18:32:05 +0900

    WIP

--- Gui/Program.fs
+++ Gui/Program.fs
@@ -1,7 +1,35 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿
+open System
+open System.Windows
+open System.Windows.Forms
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+  let onclick (sender: obj) (e: EventArgs) =
+    Forms.MessageBox.Show("on click!")
+    |> ignore
+
+  let menuItems =
+    [ ("click me", onclick)
+    ]
+    |> List.map (fun (text, h) -> new MenuItem(text, EventHandler(h)))
+    |> List.toArray
+
+  use iconMenu =
+    new ContextMenu(menuItems)
+
+  use icon =
+    new NotifyIcon
+      ( Icon         = Drawing.SystemIcons.Application
+      , Text         = "RssReaderFs"
+      , Visible      = true
+      , ContextMenu  = iconMenu
+      )
+
+  async {
+    while true do
+      do! Async.Sleep(100)
+  } |> Async.RunSynchronously
+
+  // exit code
+  0
