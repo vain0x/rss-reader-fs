954587e0bf4ee55b0d65ad6e94ccdcc17c7eda58
vain0 <id_vain@yahoo.co.jp>
2016-03-04 19:14:38 +0900

    Add `RssChecker`

---
+++ RssReaderFs/RssChecker.fs
@@ -0,0 +1,17 @@
+﻿namespace RssReaderFs
+
+open System
+
+module RssChecker =
+  type RssChecker (reader: RssReader.RssReader) =
+    inherit Observable.ObservableSource<RssItem>()
+
+    member this.RunAsync(?timeout) =
+      let timeout = defaultArg timeout (60 * 1000)
+      async {
+        while true do
+          let! newReader = reader.Update()
+          for item in newReader.Timeline do
+            this.Next(item)
+          do! Async.Sleep(timeout)
+      }
