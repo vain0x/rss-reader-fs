02e6dc50b4e6e46d02e5935a422f359d13203307
vain0 <id_vain@yahoo.co.jp>
2016-05-05 13:55:03 +0900

    Prepare to use entity framework code first

---
+++ RssReaderFs/Database.fs
@@ -0,0 +1,30 @@
+﻿namespace RssReaderFs
+
+open System.Data.Entity
+open SQLite.CodeFirst
+
+module Database =
+  type SampleDbContext() =
+    inherit DbContext("RssReaderFsDb")
+
+    override this.OnModelCreating(mb: DbModelBuilder) =
+      // configure tables
+      mb.Entity<Article       >() |> ignore
+      mb.Entity<ReadLog       >() |> ignore
+      mb.Entity<TwitterUser   >() |> ignore
+      mb.Entity<RssFeed       >() |> ignore
+      mb.Entity<Tag           >() |> ignore
+      mb.Entity<Config        >() |> ignore
+
+      Database.SetInitializer(SampleDbInitializer(mb))
+
+  and SampleDbInitializer(mb) =
+    inherit SqliteDropCreateDatabaseWhenModelChanges<SampleDbContext>(mb)
+
+[<AutoOpen>]
+module DatabaseExtension =
+  type DbCtx = Database.SampleDbContext
+
+  let withDb f =
+    use ctx = new DbCtx()
+    f ctx
