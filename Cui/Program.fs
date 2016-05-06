adef1ad3a6635cc55009fb53038cbdb1fa4bb7a2
vain0 <id_vain@yahoo.co.jp>
2016-05-06 21:49:53 +0900

    [cui] Catch exceptions

--- Cui/Program.fs
+++ Cui/Program.fs
@@ -11,20 +11,25 @@ module Program =
     let rrc = Ctrl(rc, view.PrintCommandResult)
 
     try
-      match argv with
-      | [||]
-      | [| "-i" |]
-      | [| "--interactive " |] ->
-          rrc.CheckNewItemsAsync()
-          |> Async.Start
+      try
+        match argv with
+        | [||]
+        | [| "-i" |]
+        | [| "--interactive " |] ->
+            rrc.CheckNewItemsAsync()
+            |> Async.Start
 
-          view.Interactive(rrc)
-          |> Async.RunSynchronously
-      | _ ->
-          view.PrintCommandResult(rrc.ProcCommand(argv |> Array.toList))
-          |> Async.RunSynchronously
-    finally
-      rc |> RssReader.save
+            view.Interactive(rrc)
+            |> Async.RunSynchronously
+        | _ ->
+            view.PrintCommandResult(rrc.ProcCommand(argv |> Array.toList))
+            |> Async.RunSynchronously
+      finally
+        rc |> RssReader.save
 
-    // exit code
-    0
+      // exit code
+      0
+    with
+    | e ->
+        eprintfn "%s" e.Message
+        1
